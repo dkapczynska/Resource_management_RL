8ebf0d3b7ce8632bdc7aaf7dda57cd32a3c2aad3
dkapczynska <72708718+dkapczynska@users.noreply.github.com>
2022-08-26 23:27:01 +0200

    Update distribuited_app.py

--- distribuited_app.py
+++ distribuited_app.py
@@ -5,7 +5,7 @@ import random
 from storage import *
 
 
-class DistribiutedApp(Env):
+class DistributedApp(Env):
     def __init__(self,
                  sla,
                  vm_1,
@@ -13,15 +13,14 @@ class DistribiutedApp(Env):
                  vm_3,
                  alpha=0.5,
                  beta=0.5,
-                 gamma=0.97,
-                 timeunits=60):
+                 gamma=0.97):
         """
         Custom environment that follows gym interface.
 
         Attributes
         -----------
         qos(int):
-            Quality of service metric describing how many requests were complited per seconds of simulation
+            Quality of service metric describing how many requests were completed per seconds of simulation
 
         cost(int):
             Current cost of system performance
@@ -62,12 +61,10 @@ class DistribiutedApp(Env):
         observation_space(Box object):
             Box object of class from OpenAI Gym used to describe structure of observation space
 
-        sim_lenght(int):
+        sim_length(int):
             Number of time units that simulation will last. Each step takes one time unit
 
         """
-        super(DistribiutedApp, self).__init__()
-
         self.sla = sla
         self.vm_1 = vm_1
         self.vm_2 = vm_2
@@ -77,14 +74,14 @@ class DistribiutedApp(Env):
         self.gamma = gamma
         self.request_completed = 0
         self.queue_count = 0
-        self.computing_power = 8.405
+        self.computing_power = 0
         self.state = 0
-        self.qos = 0  # actual value set in reset
-        self.cost = 0  # actual value set in reset
+        self.qos = 0
+        self.cost = 0
         self.storage = Storage({}, {}, {})
         self.action_space = Discrete(7)
-        self.observation_space = Box(low=np.array([10]), high=np.array([30]))
-        self.sim_length = timeunits
+        self.observation_space = Box(low=np.array([-500]), high=np.array([500]))
+        self.sim_length = 60
 
     def _generate_workload(self):
         """
@@ -96,32 +93,34 @@ class DistribiutedApp(Env):
         vm1_sum = self.vm_1 * (self.alpha * 2 + self.beta * 8)
         vm2_sum = self.vm_2 * (self.alpha * 3 + self.beta * 14)
         vm3_sum = self.vm_3 * (self.alpha * 5 + self.beta * 29)
-        self.computing_power = vm1_sum + vm2_sum + vm3_sum
+        self.computing_power = np.round_(vm1_sum + vm2_sum + vm3_sum, 2)
 
     def _calculate_cost(self):
         vm1_cost = self.storage.vm1['cost'] * self.vm_1
         vm2_cost = self.storage.vm2['cost'] * self.vm_2
         vm3_cost = self.storage.vm3['cost'] * self.vm_3
-        self.cost = np.sqrt(vm1_cost + vm2_cost + vm3_cost)
+        self.cost = np.round_((vm1_cost + vm2_cost + vm3_cost)/3, 2)  # np.sqrt
 
     def _calculate_utility_function(self):
         if self.qos >= self.sla:
             penalty = 0
         else:
             penalty = 10
-        self.state = self.cost + penalty
+        self.state = ((self.state + self.cost)/2) + penalty
 
     def _calculate_quality_of_service(self):
         if self.request_completed == 0:
             return 0
         else:
-            self.qos = self.request_completed / (61 - self.sim_length)
+            self.qos = np.round_(self.request_completed / (61 - self.sim_length), 4)
 
     def _calculate_reward(self):
         """
-         <====== FIX: experimental formula
+         reward clipping (values from -1  to 1)
+         so the gradient doesn't take "too big" steps
+         + discounting factor gamma <------------------ TO ADD ?
         """
-        return self.state * self.gamma
+        return np.round(self.state/1000, 2)
 
     def _apply_action(self, action):
         """
@@ -130,79 +129,28 @@ class DistribiutedApp(Env):
             1,2 - add/remove VM 1
             3,4 - add/remove VM 2
             5,6 - add/remove VM 1
-
-        To apply action:
-        0. What action was taken?
-        1. Calculate total computing power
-        2. Use the computing power to process the reqests from the queue
-        3. Remove them from the queue
-        4. One sec has passed
-        5. Calculate QoS
-        6. Calculate the cost
-        7. Calculate the utility function
-        8. Pass the state information (score of utility function)
-
-        Return state (of utility function)
         """
-        # Take an action <---------------------------------- FIX: podejrzanie wyglada ta czesc
-        if action == 0:
-            pass
+        # Take an action
+        if (action == 1) & (self.vm_1 < self.storage.vm1["capacity"]):  # add VM type I
+            self.vm_1 += 1
 
-        elif action == 1:  # add VM type I
-            if self.storage._check_if_available(1):
-                self.vm_1 += 1
-                self.storage._add_machine_from_storage(1)
-            else:
-                pass
+        elif (action == 2) & (self.vm_2 < self.storage.vm2["capacity"]):  # add VM type II
+            self.vm_2 += 1
 
-        elif action == 2:  # add VM type II
-            if self.storage._check_if_available(2):
-                self.vm_2 += 1
-                self.storage._add_machine_from_storage(2)
-            else:
-                pass
+        elif (action == 3) & (self.vm_3 < self.storage.vm3["capacity"]):  # add VM type III
+            self.vm_3 += 3
 
-        elif action == 3:  # add VM type III
-            if self.storage._check_if_available(3):
-                self.vm_1 += 3
-                self.storage._add_machine_from_storage(3)
-            else:
-                pass
-
-        elif action == 4:  # remove VM type I from the storage
+        elif (action == 4) & (self.vm_1 > 0):  # remove VM type I from system
             self.vm_1 -= 1
-            self.storage._return_machine_to_storage(1)
 
-        elif action == 5:  # remove VM type II from the storage
+        elif (action == 5) & (self.vm_2 > 0):  # remove VM type II from the system
             self.vm_2 -= 1
-            self.storage._return_machine_to_storage(2)
 
-        elif action == 6:  # remove VM type III from the storage
-            self.vm_3 = self.vm_3 - 1
-            self.storage._return_machine_to_storage(3)
+        elif (action == 6) & (self.vm_3 > 0):  # remove VM type III from the system
+            self.vm_3 -= 1
 
         else:
-            raise ValueError("Action number out of range.")
-
-        # Calculate current computing power
-        self._calculate_total_comp_power()
-
-        # Discount number of requests that were completed due to given computing power
-        vm_total_count = self.vm_1 + self.vm_2 + self.vm_3
-        requests_possible_to_complete = self.computing_power / vm_total_count
-
-        if requests_possible_to_complete > self.queue_count:
-            self.request_completed = self.queue_count
-            self.queue_count = 0
-        else:
-            self.request_completed = requests_possible_to_complete
-            self.queue_count -= self.request_completed
-
-        # Discount 1 second from run time and calculate Quality of Service for this step
-        self._calculate_quality_of_service()
-
-        # Calculate the cost for this step
-        self._calculate_cost()
+            pass
 
     def step(self, action):
         """
@@ -214,33 +162,63 @@ class DistribiutedApp(Env):
         """
         # Generate new workload fot the current step
         self._generate_workload()
-        vm_total_count = self._apply_action(action)
+
+        # perform an action
+        self._apply_action(action)
+
+        # Calculate current computing power
+        self._calculate_total_comp_power()
+
+        # Calculate current cost
+        self._calculate_cost()
+
+        # Discount number of requests that were completed due to given computing power
+        vm_total_count = self.vm_1 + self.vm_2 + self.vm_3
+        requests_possible_to_complete = np.floor(self.computing_power / vm_total_count)
+
+        if requests_possible_to_complete > self.queue_count:
+            self.request_completed = self.queue_count
+            self.queue_count = 0
+        else:
+            self.request_completed = requests_possible_to_complete
+            self.queue_count -= self.request_completed
+
+        # calculate Quality of Service
+        self._calculate_quality_of_service()
+
+        # calculate the reward
         reward = self._calculate_reward()
+
+        # calculate next state
+        self._calculate_utility_function()
 
         # Check termination condition
         self.sim_length -= 1
         if self.sim_length <= 0:
             done = True
+            reward = 0
         else:
             done = False
 
         info = {'Quality of Service ': self.qos,
-                'Total cost': self.cost,
+                'Current cost': self.cost,
                 'TCP': self.computing_power,
                 'VM type I': self.vm_1,
                 'VM type II': self.vm_2,
                 'VM type III': self.vm_3,
                 'No of VMs': vm_total_count,
                 }
-        return self.cost, reward, done, info
+
+        return np.array([self.state, ], dtype=np.float32), reward, done, info
 
     def reset(self, **kwargs):
         # create storage
-        self.storage = Storage(vm1={"cpu": 10, "memory": 50, "capacity": self.vm_1, "cost": 25},
-                               vm2={"cpu": 17, "memory": 65, "capacity": self.vm_2, "cost": 35},
-                               vm3={"cpu": 25, "memory": 90, "capacity": self.vm_3, "cost": 75})
+        self.storage = Storage(vm1={"cpu": 10, "memory": 50, "capacity": 25, "cost": 20},
+                               vm2={"cpu": 17, "memory": 65, "capacity": 18, "cost": 35},
+                               vm3={"cpu": 25, "memory": 90, "capacity": 10, "cost": 75})
 
-        # reset the queue
+        # reset the queue and the clock
+        self.sim_length = 60
         self.request_completed = 0
         self.queue_count = 0
 
@@ -249,4 +227,13 @@ class DistribiutedApp(Env):
         self._calculate_quality_of_service()
         self._calculate_cost()
         self._calculate_utility_function()
-        return self.state
+        return np.array([self.state, ], dtype=np.float32)
+
+    def render(self, **kwargs):
+        pass
+
+    def close(self):
+        """
+        Clean up <------------------- what should be included here?
+        """
+        return 0
