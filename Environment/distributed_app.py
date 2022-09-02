6569bc88ec7500540b70f6a7b1792f3a30392ff0
dkapczynska <72708718+dkapczynska@users.noreply.github.com>
2022-09-02 17:05:39 +0200

    add: scenario choice possibility

--- Environment/distributed_app.py
+++ Environment/distributed_app.py
@@ -1,6 +1,7 @@
 from multiprocessing import Process, Queue
 import time
 import warnings
+
 warnings.filterwarnings("ignore")
 
 
@@ -18,28 +19,24 @@ class DistributedApp:
 
         cpu = self.resources[machine_type][0]
         memory = self.resources[machine_type][1]
-        time_out = 10 * request_size/(self.alpha * cpu + self.beta * memory)  # the bigger the resources the smaller time out
+        time_out = 10 * request_size / (
+                    self.alpha * cpu + self.beta * memory)  # the bigger the resources the smaller time out
         time.sleep(time_out)
 
-    def _worker_node_proc(self, queue, done_q, machine_type):  
+    def _worker_node_proc(self, queue, done_q, machine_type):
         """Take from the queue; this spawns as a separate Process"""
-        try:
-            while True:
-                queue_item = queue.get()
-                # assert 0 < queue_item < 50
-                if str(queue_item) == "DONE":
-                    done_q.put(queue_item)
-                    break
-                else:
-                    self._complete_request(queue_item, machine_type)
-        except:
-            print("Broken pipe")
-            
+        while True:
+            queue_item = queue.get()
+            if str(queue_item) == "DONE":
+                done_q.put(queue_item)
+                break
+            else:
+                self._complete_request(queue_item, machine_type)
         return True
 
     def _master_node_proc(self, count, queue):
         """Write integers into the queue.  A reader_proc() will read them from the queue"""
-        for ii in range(0, count-1):
+        for ii in range(0, count - 1):
             queue.put(count)  # Put 'count' numbers into queue
 
         for ii in range(0, len(self.machines)):  # Tell all workers to stop...
@@ -60,20 +57,19 @@ class DistributedApp:
     def _perform_all_requests(self):
         qq = Queue()
         done_q = Queue()
-
         start = time.time()
+
         # prepare the queue
         for count in self.queue_array:
             self._master_node_proc(count, qq)
         # run all worker processes
-        # assert 0 < len(self.machines) < 53
         all_worker_procs = self._start_worker_procs(qq, done_q)
 
         # wait the processes to finish
         for _, a_worker_proc in enumerate(all_worker_procs):
             a_worker_proc.join()  # Wait for worker node to finish
             # print("Finished:", a_worker_proc)
-            
+
         done_q.close()
         qq.close()
 
